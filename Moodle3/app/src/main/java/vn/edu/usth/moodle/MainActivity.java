6f7338a029d6db503f3c40f4dc37e3fd7ba4e02e
thanhhieudo278 <74595346+thanhhieudo278@users.noreply.github.com>
2021-10-16 23:38:49 +0700

    push

---
+++ Moodle3/app/src/main/java/vn/edu/usth/moodle/MainActivity.java
@@ -0,0 +1,102 @@
+package vn.edu.usth.moodle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create a new Fragment to be placed in the activity layout
+
+
+        // Create a new Fragment to be placed in the activity layout
+
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+        Log.i(TAG, "onStart");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(TAG, "onResume");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.haha1:
+                Toast.makeText(this, "haha 1 selected", Toast.LENGTH_SHORT).show();
+
+            case R.id.refresh:
+                Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT).show();
+
+            case R.id.search:
+                Toast.makeText(this, "Search selected", Toast.LENGTH_SHORT).show();
+
+            case R.id.setting:
+                Toast.makeText(this, "setting selected", Toast.LENGTH_SHORT).show();
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
